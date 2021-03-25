76bd104c989141b26d94fb36bdd8f4deee55b306
glennp1 <61995051+glennp1@users.noreply.github.com>
2021-03-25 11:55:30 +1100

    main.c now parses in command line arguements and prints them out

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 //
 
 // --- System Libraries ---
-#include <stdio.h>
+#include <stdio.h> // for input / output
+#include <stdlib.h> // for atoi
+#include <stdbool.h> // for booleans
+#include <string.h> // for strcmp
 
 // --- Project Libraries ---
 #include "hello.h"
@@ -22,6 +25,52 @@ int main(int argc, char *argv[]) {
     printf("%s\n", STARTING);
 
     say_hello();
+
+    // todo move this to somewhere better
+    // store the filename here
+    char* filename = "blank";
+
+    // store the processors here
+    int num_processors = -1;
+
+    // store the scheduler here
+    bool own_scheduler = false;
+
+    // parse in command line arguments
+    // todo good enough for now
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-f") == 0) {
+
+            // parse in processes.txt
+            i++;
+            // todo add guard???
+            filename = argv[i];
+        }
+        else if (strcmp(argv[i], "-p") == 0) {
+            // parse in number of processors
+            i++;
+            // todo add guard???
+            num_processors = atoi(argv[i]);
+        }
+        else if (strcmp(argv[i], "-c") == 0) {
+            // toggle own scheduler has been invoked
+            own_scheduler = true;
+        }
+        else {
+            // todo handle error
+            // can print to stderr??? rather than stdout which is assessed
+        }
+
+    }
+
+    printf("filename: %s\n", filename);
+    printf("number of processors: %d\n", num_processors);
+    printf("own scheduler: %d\n", own_scheduler);
+
+    // ongoing output
+
+
+    // print final results
 
     return 0;
 }
