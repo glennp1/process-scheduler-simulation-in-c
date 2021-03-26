0da9ebf3e2ad9f58bc537b1e8494a6ed9f892abf
glennp1 <61995051+glennp1@users.noreply.github.com>
2021-03-26 11:16:53 +1100

    Commented a basic project skeleton for a single processor

--- main.c
+++ main.c
@@ -18,19 +18,49 @@
 // --- Function Implementations ---
 int main(int argc, char *argv[]) {
 
-    // initialise input
+    // handle and store input arguments
     input_arguments_t input = handle_input(argc, argv);
-
-    //
-    processes_list
-
 
     // todo temporary output
     printf("filename: %s\n", input.filename);
     printf("number of processors: %d\n", input.processors);
     printf("own scheduler: %s\n", input.challenge ? "true" : "false");
 
+    // read in processes
+    // todo save this in queue
     read_in_processes(input.filename);
+
+    // execute
+
+    // tick = 0
+    // while there are still processes to execute
+
+        // for each in running list
+            // check its current process has finished
+            // if it has
+                // (1) change its state to FINISHED, add it to the finished list
+            // if it has not
+                // (2) add it to the waiting list and change its state to WAITING
+                // this ensures both current and new processes are considered together
+
+        // while head of queue matches the tick
+            // (3) dequeue the head, change its state to WAITING and add it to waiting list data structure
+            // todo determine which data structure is appropriate for waiting (needs to be sorted)
+
+        // (4) sort the waiting list based on whatever allocator is currently in use
+
+        // for each processor in processors
+            // (5) add the head of the waiting list to the running list, change state to RUNNING
+
+        // (6) sort finished, print
+        // ()
+
+        // increment tick
+        // for each processor
+            // (7) decrease the time remaining on it's respective process
+
+    // END OF WHILE
+
 
     // ongoing output
 
