412bb759424a28558af066eb15fdf8c9b6aa39cb
glennp1 <61995051+glennp1@users.noreply.github.com>
2021-03-27 20:37:23 +1100

    added further planning to main

--- main.c
+++ main.c
@@ -48,6 +48,15 @@ int main(int argc, char *argv[]) {
     // free input data
     free_input(input);
 
+    // todo remove remaining processes
+//    process_t *removed_process;
+//    while(!priority_queue_is_empty(queuing_processes)) {
+//
+//        // remove a process and free it
+//        removed_process = priority_queue_remove_min(queuing_processes);
+//        free_process(removed_process);
+//    }
+
     return 0;
 }
 
@@ -70,57 +79,50 @@ void start_simulation(priority_queue_t *queuing_processes) {
 
 // helper function that performs one tick (second) of the simulation
 void perform_one_simulation_tick(priority_queue_t *queuing_processes, unsigned int curr_tick) {
-    process_t *removed_process;
+    process_t *queued_process;
 
     printf("** tick: %u **\n", curr_tick);
 
-//
-//
-//    // todo remove remaining processes
-//    while(!priority_queue_is_empty(queuing_processes)) {
-//
-//        // remove a process and print it
-//        removed_process = priority_queue_remove_min(queuing_processes);
-//
-//        printf("%u %u %u %c\n", removed_process->time_arrived,
-//               removed_process->process_id, removed_process->execution_time,
-//               removed_process->parallelisable ? 'p' : 'n');
-//        free_process(removed_process);
-//    }
+    // todo could potentially use queue of queues?
+    // (1)
+    // for each CPU
+        // if the running process has finished (time remaining = 0)
+            // remove it from running
+            // add it to the finished queue
+            // add it to the finished this tick queue
 
-    // for each in running list
-    // check its current process has finished
-    // if it has
-    // (1) change its state to FINISHED, add it to the finished list
-    // if it has not
-    // (2) add it to the waiting list and change its state to WAITING
-    // this ensures both current and new processes are considered together
-
-    // while head of queue matches the tick
-    // (3) dequeue the head, change its state to WAITING and add it to waiting list data structure
+    // todo find a better name than queuing processes
+    // while the future? processes queue is not empty
+    // (3) move all processes that match the current tick to the waiting queue
 
     while (!priority_queue_is_empty(queuing_processes)) {
-        removed_process = priority_queue_remove_min_if_equals(
+        queued_process = priority_queue_remove_min_if_equals(
                 queuing_processes, curr_tick);
 
-        if (removed_process != NULL) {
+        if (queued_process != NULL) {
 
-            // todo do something...
+            // todo add the process to the waiting queue
+            // todo priority = time remaining
 
             // todo temporary print
-            printf("%u %u %u %c\n", removed_process->time_arrived,
-                   removed_process->process_id, removed_process->execution_time,
-                   removed_process->parallelisable ? 'p' : 'n');
+            printf("%u %u %u %c\n", queued_process->time_arrived,
+                   queued_process->process_id, queued_process->execution_time,
+                   queued_process->parallelisable ? 'p' : 'n');
 
             // todo
             // free the process - only if done
-            free_process(removed_process);
+            free_process(queued_process);
         }
         else {
             break;
         }
     }
 
+    // get all the shortest processes in waiting queue
+        // add them to express queue???
+
+    // get all the longest processes in running
+        // add them to rejects queue??? (priority = ID)
 
 
     // (4) sort the waiting list based on whatever allocator is currently in use
@@ -133,17 +135,14 @@ void perform_one_simulation_tick(priority_queue_t *queuing_processes, unsigned i
     // for each processor in processors
     // (5) add the head of the waiting list to the running list, change state to RUNNING
 
-    // (6) sort finished, print
-    // ()
+    // todo ongoing output
+    // (6.1) sort finished this tick, print (priority = id)
+
+    // (6.2) sort added this tick, print (priority = id)
 
     // increment tick
     // for each processor
-    // (7) decrease the time remaining on it's respective process
-
-    // todo ongoing output
-
-
-    // END OF WHILE
+        // (7) decrease the time remaining on it's respective process
 
 
 }
